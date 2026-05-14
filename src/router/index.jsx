4747a24d8bd40e4a192a005f9062b21c6b44d1d9
Shubham Vairagade <shubhamvairagade91@gmail.com>
2026-05-14 09:45:28 +0530

    Route setup done for all pages

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,6 +1,16 @@
 import { createBrowserRouter, Navigate} from "react-router-dom";
-import {ROUTES} from '../constants/routes';
 
+import { ROUTES } from "../constants/routes";
+import AuthLayout from '../components/layout/AuthLayout'
+import LoginPage from "../pages/LoginPage";
+import RegisterPage from '../pages/RegisterPage';
+import DashboardLayout from '../components/layout/DashboardLayout';
+import DashboardPage from '../pages/DashboardPage';
+import LeadsPage from '../pages/LeadsPage';
+import ContactsPage from '../pages/ContactsPage';
+import ReprotsPage from '../pages/ReportsPage';
+import SettingsPage from '../pages/SettingsPage';
+import NotFoundPage from '../pages/NotFoundPage';
 
 
 export const router = createBrowserRouter([
@@ -9,6 +19,25 @@ export const router = createBrowserRouter([
         element:<Navigate to={ROUTES.LOGIN} replace/>      
     },
     {
-        
+        element:<AuthLayout/>,
+        children:[
+            {path:ROUTES.LOGIN, element:<LoginPage/>},
+            {path:ROUTES.REGISTER, element:<RegisterPage/>}
+        ]
+    },
+    {
+        element:<DashboardLayout/>,
+        children:[
+            {path:ROUTES.DASHBOARD, element:<DashboardPage/>},
+            {path:ROUTES.LEADS, element:<LeadsPage/>},
+            {path:ROUTES.CONTACTS, element:<ContactsPage/>},
+            {path:ROUTES.REPORTS, element:<ReprotsPage/>},
+            {path:ROUTES.SETTINGS, element:<SettingsPage/>}
+        ]
+    },
+    {
+        path:"*",
+        element:<NotFoundPage/>
     }
+
 ])
